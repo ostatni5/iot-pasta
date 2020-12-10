c8618f3e3db03d7b1a088399f1113994e6b4dca3
Michał Krawczyk <michaskrawczyk@tlen.pl>
2020-12-10 01:44:07 +0100

    edit run.bat

--- run.bat
+++ run.bat
@@ -1,16 +1,25 @@
 START /b python orderer.py 
 
-START /b python fmixer.py 
+ping 127.0.0.1 -n 2 > nul
+START /b python fmixer.py
+ping 127.0.0.1 -n 2 > nul 
 START /b python steamer.py 
+ping 127.0.0.1 -n 2 > nul 
 START /b python mixer.py 
+ping 127.0.0.1 -n 2 > nul 
 START /b python pipeline.py 
+ping 127.0.0.1 -n 2 > nul 
 START /b python dryer.py 
+ping 127.0.0.1 -n 2 > nul 
 START /b python belt.py 
+ping 127.0.0.1 -n 2 > nul 
 START /b python lift.py 
+ping 127.0.0.1 -n 2 > nul 
 @REM START /b python scale.py 
 @REM START /b python cooler.py 
 @REM START /b python silos.py 
 
+ping 127.0.0.1 -n 2 > nul 
 START python logger.py
 
 
