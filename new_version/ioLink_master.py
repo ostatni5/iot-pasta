5866d5a44b2fdfa70c671a2c1b6bf3cb54a2fd4c
Arkadiusz <arkadiusz.cwikla98@gmail.com>
2021-01-20 22:41:16 +0100

    update master

--- new_version/ioLink_master.py
+++ new_version/ioLink_master.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+# inspired by IO-Link master with EtherNet/IP interface AL1322 made by ifm electronic gmbh
+# https://www.ifm.com/de/en/product/AL1322
+
+
+
 
 class Master:
     def __init__(self, name):
         self.name = name
+        self.sensors = []
         # TODO
 
